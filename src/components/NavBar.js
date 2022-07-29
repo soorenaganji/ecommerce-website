a32ddeb7be85a39f2904f4d823aedcd42ec5317e
soorenaganji <109369184+soorenaganji@users.noreply.github.com>
2022-07-29 04:36:57 +0430

    ssxss

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,15 +2,21 @@ import React from 'react'
 import styles from './NavBar.module.css'
 import logo from '../logo.png'
 export default function NavBar() {
-  return (<> <div className={styles.headerImage} > </div>
+ const clickHandler = () => {
+window.open("https://google.com")
+  }
+  return (<> 
+        <div className={styles.headerImage} > </div>
     <div className={styles.main} >
       <ul className={styles.navContainer} >
         <li className={styles.nav}>Home</li>
         <li className={styles.nav}>Products</li>
         <li className={styles.nav}>Contact us</li>
       </ul>
-      <input type={"search"} placeholder='Search ...' className={styles.searchBar} />
-      <img src={logo}  className={styles.logo} />
+        <input type={"search"} placeholder='Search ...' className={styles.searchBar} />
+        <button onClick={clickHandler} className={styles.btn} >login / signup</button>
+        <img src={logo}  className={styles.logo} />
+      
     </div>
   
        </>
